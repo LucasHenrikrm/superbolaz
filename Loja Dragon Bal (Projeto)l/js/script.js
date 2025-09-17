85aa9a9589ace6808622dae2dc8267afb8ffeb72
Leonardo Ribeiro - CØdacio <leo.fernandez.ribeiro@gmail.com>
2025-09-17 13:42:52 -0300

    Estrutura inicial do projeto
    - Criação e organização da estrutura de diretórios (css, js, images).
    - Adição do arquivo index.html com semântica HTML5 bem definida.
    - Linkagem dos arquivos CSS customizado, JavaScript e do framework Bootstrap.
    - Inclusão de um banco de imagens inicial com 12 assets na pasta /images.
    - Adição de comentários iniciais no código para clareza e futuras implementações.

---
+++ Loja Dragon Bal (Projeto)l/js/script.js
@@ -0,0 +1,10 @@
+// ↓ Funcao para criar espaco de armazenamento da Chave de login e Senha da Chave
+function cadastro() {
+    
+}
+
+// ↓ Funcao para validar a chave de login e senha da chave 
+function login() {
+
+}
+
